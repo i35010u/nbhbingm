95aa21c880d88ace7f9d00f79b8c814ff5181c80
TheZihanGu <i@zihangu.cn>
2020-05-16 18:34:14 +0800

    主页 - 大修改

--- js/index.js
+++ js/index.js
@@ -1,19 +1,17 @@
 /**
- *  Designed by WolfBolin
- *  https://wolfbolin.com
- *  mailto@wolfbolin.com
+ * TheZihanGu/nbhbdm
+ * https://nbhbdm.cn
+ * 原项目: WolfBolin/baidux
  */
 // 剪切板功能模块
 var clipboard = new ClipboardJS('.copy');
 clipboard.on('success', function(e) {
     console.log(e);
-    $("#tip1").text("已将链接复制到剪切板");
-    $("#tip2").text("快把这智慧的链接糊给他吧");
+    $("#tip1").text("已将链接复制到剪贴板");
 });
 clipboard.on('error', function(e) {
     console.log(e);
     $("#tip1").text("不知道为啥复制失败了");
-    $("#tip2").text("你可以手动复制框内的文本");
 });
 // 通过正则表达式获取链接中的参数
 function getQuery(name) {
@@ -49,8 +47,7 @@ function gotoBeginStatus() {
     mouse_obj = $("#fake-mouse");
     mouse_obj.stop(true);
     mouse_obj.css("display", "none");
-    $("#tip1").text("我帮您教他如何使用百度搜索");
-    $("#tip2").text("将问题输入后点击\"百度一下\"");
+    $("#tip1").text("输入搜索关键字");
     $("#link").css("display", "none");
 }
 // 用户点击输入框时终止所有动画
@@ -67,6 +64,13 @@ $("#search-button").mouseup(function() {
 });
 // “百度一下”按钮的响应事件
 $("#search-button").click(function() {
+    input_text = $("#search-input").val();
+    input_text = input_text.trim();
+    query_text = encodeURIComponent(input_text);
+    $(location).attr('href', hostname + "?s=" + query_text);
+});
+// "我要生成链接" 响应事件
+$("#generate").click(function() {
     // 获取输入框输入内容
     input_text = $("#search-input").val();
     input_text = input_text.trim();
@@ -75,16 +79,31 @@ $("#search-button").click(function() {
     // 根据输入框内容更新显示样式
     if (input_text.length !== 0) {
         $("#tip1").text("蕴藏知识的链接已经生成");
-        $("#tip2").text("点击\"复制链接\"即可复制到剪切板");
-        $("#link").css("display", "block");
+        $("#result").css("display", "block");
+    }
+    if (input_text.length == 0) {
+        $("#tip1").text("请正确输入搜索内容");
     }
     query_text = encodeURIComponent(input_text);
-    $("#tip-input").val(hostname + "?q=" + query_text);
+    $("#bd-link").text(hostname + "?s=" + query_text);
+    $('#short-link').text("正在生成中")
+    document.getElementById("bd-link").href = hostname + "?s=" + query_text;
+    var httpRequest = new XMLHttpRequest();
+        httpRequest.open('GET', "https://api.tzg6.com/api/shortlink?token=nbhbdm&url=" + hostname + "?s=" + query_text, true);
+        httpRequest.send();
+        httpRequest.onreadystatechange = function () {
+            if (httpRequest.readyState == 4 && httpRequest.status == 200) {
+                var json = httpRequest.responseText;
+                console.log(json);
+                $("#short-link").text(json)
+                document.getElementById("bd-link").href = json;
+            }
+        }
 });
 // 主要业务逻辑
 $(document).ready(function() {
     hostname = getHostname(); // 获取页面域名或地址
-    query = getQuery("q"); // 试图获取查询参数
+    query = getQuery("s"); // 试图获取查询参数
     if (query == null) {
         // 没有参数输入，中断处理
         return;
@@ -97,8 +116,7 @@ $(document).ready(function() {
         // 参数不为空时呈现动态
         // 更新输入框与提示框内容
         $("#search-input").val("");
-        $("#tip1").text("其实找资料特别简单呢");
-        $("#tip2").text("跟着我一起输入你想知道的问题");
+        $("#tip1").text("将光标移动到搜索框上并点击");
         // 获取输入框与按钮的位置
         input_offest = $("#search-input").offset();
         button_offest = $("#search-button").offset();
@@ -119,7 +137,7 @@ $(document).ready(function() {
             "left": input_offest.left + 10,
         }, 2000, "swing", function() {
             // 计算每个字符的输入时间
-            $("#tip2").text("输入之后点击\"百度一下\"按钮");
+            $("#tip1").text("输入要搜索的内容后点击\"百度一下\"按钮");
             time = 1800 / query.length;
             addSearchText(query, time); // 逐个写入文本内容
         });
@@ -129,7 +147,7 @@ $(document).ready(function() {
             "left": button_offest.left + 90
         }, 2000, "swing", function() {
             $("#search-button").addClass("wb-click");
-            $("#tip2").text("然后就能找到你想要的答案啦");
+            $("#tip1").text("是不是很简单呢");
             navigate = "https://www.baidu.com/s?wd=";
             navigate += encodeURIComponent(query);
             console.log("百度一下")
